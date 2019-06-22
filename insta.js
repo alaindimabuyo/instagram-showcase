d0ef725dc181304a7f4c759e449e4b80e9895476
Alain Dimabuyo <xalaindimabuyo@gmail.com>
2019-06-22 16:22:59 +0800

    token

--- insta.js
+++ insta.js
@@ -1,28 +1,17 @@
-class instagram{
-    async get(){
-        const res = await fetch('https://api.instagram.com/v1/users/self/media/recent/?access_token=572689804.63f8a73.e5efe8f25c0e4f779be15e11467498be');
-        const resAlain = await fetch('https://api.instagram.com/v1/users/self/?access_token=572689804.63f8a73.e5efe8f25c0e4f779be15e11467498be');
-        const data = await res.json()
-        const dataAlain = await resAlain.json()
-        
-        return {
-            data,
-            dataAlain
-        }
-    }
+class instagram {
+  async get() {
+    const res = await fetch(
+      "https://api.instagram.com/v1/users/self/media/recent/?access_token=572689804.f6c0c6a.7450f1a2df6441d08642f355e12b1981"
+    );
+    const resAlain = await fetch(
+      "https://api.instagram.com/v1/users/self/?access_token=572689804.f6c0c6a.7450f1a2df6441d08642f355e12b1981"
+    );
+    const data = await res.json();
+    const dataAlain = await resAlain.json();
+
+    return {
+      data,
+      dataAlain
+    };
+  }
 }
-
-
-
-
-
-
-
-        
-
-  
-
-
-
-
-
